e2ffedc62d62e66a2def89f67393c4f810005282
Stiven-Yepes-Arenas <estivenyepesarenas@gmail.com>
2023-08-01 13:42:50 -0500

    organice la estructura de las carpetas con la carpeta src

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 const express = require ("express");
+
 const app = express();
+
 const path = require('path');
-const publicPath =path.resolve(__dirname, './public');
+
+const publicPath =path.resolve(__dirname, '../public');
 app.use(express.static(publicPath))
 
 app.listen(3000,()=>{
@@ -9,5 +12,5 @@ app.listen(3000,()=>{
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, 'views/home.html'));
 });
