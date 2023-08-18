e5df17dd31c0283e2864bf945112a2edd87a90d8
Stiven-Yepes-Arenas <estivenyepesarenas@gmail.com>
2023-08-17 21:00:21 -0500

    ahora tengo una base usando flexbos para hacer responsive, adelante algunos formularios y agregue diseños con css

--- src/app.js
+++ src/app.js
@@ -7,10 +7,17 @@ const path = require('path');
 const publicPath =path.resolve(__dirname, '../public');
 app.use(express.static(publicPath))
 
+
 app.listen(3000,()=>{
     console.log('SERVIDOR CORRIENDO')
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
+});
+app.get('/register', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/register.html'));
+});
+app.get('/login', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'views/login.html'))
 });
