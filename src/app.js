ded6f8558076e012525b6b7603a0f877266e7966
Stiven-Yepes-Arenas <estivenyepesarenas@gmail.com>
2023-08-21 19:13:32 -0500

    puse el puerto en variable

--- src/app.js
+++ src/app.js
@@ -1,16 +1,14 @@
 const express = require ("express");
-
+const path = require('path');
 const app = express();
 
-const path = require('path');
+app.use(express.static(path.join(__dirname, "../public")));
 
-const publicPath =path.resolve(__dirname, '../public');
-app.use(express.static(publicPath))
-
-
-app.listen(3000,()=>{
-    console.log('SERVIDOR CORRIENDO')
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Se prendió en el puerto ${PORT}`);
 });
+
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'views/home.html'));
